a88fc57eb2574f1ed499f76dc52d46fbf56bbcb9
Nathan Hamblen <nathan@technically.us>
2011-02-20 11:26:08 -0500

    rendered show

---
+++ stream_files/custom.js
@@ -0,0 +1,21 @@
+/* custom behavior */
+
+$(function() {
+    var socket = new WebSocket("ws://stream.meetup.com/2/rsvps");
+
+    var ticker = $('<div id="ticker"/>');
+    $(document.body).append(ticker);
+  
+    socket.onmessage = function(event) {
+        var rsvp = JSON.parse(event.data);
+        if (rsvp.response != "yes") return;
+        var span = $(["<div><span>", rsvp.group.group_name, "</span></div>"].join(""));
+        ticker.append(span);
+        var childs = ticker.children();
+        var MAX = 10; // max we think might still be on screen
+        childs.each(function(idx) {
+            if (idx < childs.size() - MAX) $(this).remove();
+        });
+        span.animate({ width: 'show' }, 1000);
+    };
+});
